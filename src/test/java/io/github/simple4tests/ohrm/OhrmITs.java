0561d760ae7fabe002e135aa83c06567d4a42769
frederic.gs <frederic.gs@gmail.com>
2022-10-07 14:57:28 +0200

    in progress

--- src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
+++ src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
@@ -7,6 +7,8 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, html:target/site/cucumber/index.html")
+@ConfigurationParameter(
+        key = Constants.PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/site/cucumber/index.html")
 public class OhrmITs {
 }
