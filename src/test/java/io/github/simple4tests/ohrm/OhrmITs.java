dc3d66c2936ed3669e49939ac785dfd0546ed133
frederic.gs <frederic.gs@gmail.com>
2023-05-03 23:16:38 +0200

    in progress

--- src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
+++ src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
@@ -1,10 +1,19 @@
 package io.github.simple4tests.ohrm;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import io.cucumber.core.options.Constants;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features")
+//@RunWith(CucumberWithSerenity.class)
+//@CucumberOptions(features = "src/test/resources/features")
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = Constants.PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/site/cucumber/index.html")
+@ExtendWith(SerenityJUnit5Extension.class)
 public class OhrmITs {
 }
