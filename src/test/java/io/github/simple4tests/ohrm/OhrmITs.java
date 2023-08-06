3d2f2b2fedda63158ee1e98048f009458e26e74a
frederic.gs <frederic.gs@gmail.com>
2023-08-06 13:28:02 +0200

    in progress

--- src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
+++ src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
@@ -2,10 +2,12 @@ package io.github.simple4tests.ohrm;
 
 import io.cucumber.core.options.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(
