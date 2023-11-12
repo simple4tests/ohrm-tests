dc7d40a6e762b7ac00584cf194b742ed787ba9b4
frederic.gs <frederic.gs@gmail.com>
2023-11-12 22:32:02 +0100

    review

--- src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
+++ src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
@@ -10,16 +10,16 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
-@ConfigurationParameter(key = "cucumber.execution.parallel.enabled", value = "false")
-@ConfigurationParameter(key = "cucumber.execution.parallel.config.strategy", value = "fixed")
-@ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.parallelism", value = "3")
-@ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.max-pool-size", value = "3")
-@ConfigurationParameter(key = "cucumber.execution.execution-mode.feature", value = "same_thread")
-@ConfigurationParameter(
-        key = "cucumber.execution.exclusive-resources.isolated.read-write",
-        value = "org.junit.platform.engine.support.hierarchical.ExclusiveResource.GLOBAL_KEY")
 @ConfigurationParameter(
         key = Constants.PLUGIN_PROPERTY_NAME,
         value = "pretty,json:target/cucumber.json,io.cucumber.core.plugin.SerenityReporter")
+//@ConfigurationParameter(key = "cucumber.execution.parallel.enabled", value = "false")
+//@ConfigurationParameter(key = "cucumber.execution.parallel.config.strategy", value = "fixed")
+//@ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.parallelism", value = "3")
+//@ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.max-pool-size", value = "3")
+//@ConfigurationParameter(key = "cucumber.execution.execution-mode.feature", value = "same_thread")
+//@ConfigurationParameter(
+//        key = "cucumber.execution.exclusive-resources.isolated.read-write",
+//        value = "org.junit.platform.engine.support.hierarchical.ExclusiveResource.GLOBAL_KEY")
 public class OhrmITs {
 }
