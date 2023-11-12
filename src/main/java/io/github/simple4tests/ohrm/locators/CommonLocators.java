e07003a93b32c9c6558118ebef5e0f1f493a0b09
frederic.gs <frederic.gs@gmail.com>
2023-11-12 20:31:23 +0100

    in progress

--- src/main/java/io/github/simple4tests/ohrm/locators/CommonLocators.java
+++ src/main/java/io/github/simple4tests/ohrm/locators/CommonLocators.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 
 public class CommonLocators {
 
-    public static final By LOADER = By.xpath("//*[@class='oxd-loading-spinner']");
+    public static final By CONTENT = By.xpath("//div[@id='app' and @data-v-app]");
+    public static final By SPINNER = By.xpath("//*[@class='oxd-loading-spinner']");
+    public static final By LOADER = By.xpath("//*[@class='oxd-form-loader']");
 
     public static final By TITLE = By.xpath("//header//span/h6");
 
