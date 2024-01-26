8dddbd102f31d95427a990ece3af3d6f141715af
frederic-gs <frederic.gs@gmail.com>
2024-01-26 23:05:44 +0100

    rework

--- src/main/java/io/github/simple4tests/ohrm/capabilities/Buzz.java
+++ src/main/java/io/github/simple4tests/ohrm/capabilities/Buzz.java
@@ -2,21 +2,32 @@ package io.github.simple4tests.ohrm.capabilities;
 
 import io.github.simple4tests.ohrm.Ohrm;
 
-import static io.github.simple4tests.ohrm.locators.BuzzLocators.*;
-
 public class Buzz extends Ohrm {
 
+    public static final String NEW_MESSAGE = "//*[@placeholder=\"What's on your mind?\"]";
+    public static final String POST = "//button[contains(., 'Post') and not(contains(., 'Recent'))]";
+
+    public static final String MESSAGE_X = "//*[contains(@class,'orangehrm-buzz-post-body-text') and text()='%s']";
+    public static final String MESSAGE_X_Y = "(//*[@id='buzz']//li[.//*[@id='postEmployeeName' and normalize-space()='%s']]//textarea[contains(@id, 'commentBoxNew_listId') and @class='commentBox'])[%d]";
+    public static final String POST_X_Y = "(//*[@id='buzz']//li[.//*[@id='postEmployeeName' and normalize-space()='%s']]//textarea[contains(@id, 'commentBoxNew_listId') and @class='commentBox']//following-sibling::input[@type='button'])[%d]";
+
     public void add(String message) {
-        reporter.reportAction("Add message : ".concat(message));
+        reporter.startAction("Add message : ".concat(message));
         ui.sendKeys(NEW_MESSAGE, message);
         ui.click(POST);
         ui.waitForPageToLoad();
+        reporter.endAction();
+    }
+
+    public boolean waitToBePresent(String message) {
+        return ui.waitToBePresent(String.format(MESSAGE_X, message), true);
     }
 
     public void reply(String user, int postNumber, String message) {
-        reporter.reportAction("Reply : ".concat(message));
+        reporter.startAction("Reply : ".concat(message));
         ui.sendKeys(String.format(MESSAGE_X_Y, user, postNumber), message);
         ui.click(String.format(POST_X_Y, user, postNumber));
         ui.waitForPageToLoad();
+        reporter.endAction();
     }
 }
--- src/main/java/io/github/simple4tests/ohrm/capabilities/Login.java
+++ src/main/java/io/github/simple4tests/ohrm/capabilities/Login.java
@@ -1,15 +1,19 @@
 package io.github.simple4tests.ohrm.capabilities;
 
 import io.github.simple4tests.ohrm.Ohrm;
-
-import static io.github.simple4tests.ohrm.locators.LoginLocators.*;
+import org.openqa.selenium.By;
 
 public class Login extends Ohrm {
 
+    public static final By USERNAME = By.xpath("//input[@name='username']");
+    public static final By PASSWORD = By.xpath("//input[@name='password']");
+    public static final By LOGIN = By.xpath("//button[@type='submit']");
+
     public void signIn(String username, String password) {
-        reporter.reportAction("Sign in with username ".concat(username));
+        reporter.startAction("Sign in with username ".concat(username));
         ui.sendKeys(USERNAME, username);
         ui.sendKeys(PASSWORD, password);
         ui.click(LOGIN);
+        reporter.endAction();
     }
 }
