6270e398167975f7b8a6bd8f9d1e08110833cb61
frederic-gs <frederic.gs@gmail.com>
2024-02-02 13:56:48 +0100

    rework

--- src/main/java/io/github/simple4tests/ohrm/capabilities/Buzz.java
+++ src/main/java/io/github/simple4tests/ohrm/capabilities/Buzz.java
@@ -12,11 +12,11 @@ public class Buzz extends Ohrm {
     public static final String POST_X_Y = "(//*[@id='buzz']//li[.//*[@id='postEmployeeName' and normalize-space()='%s']]//textarea[contains(@id, 'commentBoxNew_listId') and @class='commentBox']//following-sibling::input[@type='button'])[%d]";
 
     public void add(String message) {
-        reporter.startAction("Add message : ".concat(message));
+        reporter.startStep("Add message : ".concat(message));
         ui.sendKeys(NEW_MESSAGE, message);
         ui.click(POST);
         ui.waitForPageToLoad();
-        reporter.endAction();
+        reporter.endStep();
     }
 
     public boolean waitToBePresent(String message) {
@@ -24,10 +24,10 @@ public class Buzz extends Ohrm {
     }
 
     public void reply(String user, int postNumber, String message) {
-        reporter.startAction("Reply : ".concat(message));
+        reporter.startStep("Reply : ".concat(message));
         ui.sendKeys(String.format(MESSAGE_X_Y, user, postNumber), message);
         ui.click(String.format(POST_X_Y, user, postNumber));
         ui.waitForPageToLoad();
-        reporter.endAction();
+        reporter.endStep();
     }
 }
