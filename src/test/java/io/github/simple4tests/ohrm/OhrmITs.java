854a87275f2c8a47b68e0206945be59c10b246b1
frederic-gs <frederic.gs@gmail.com>
2024-06-26 21:45:36 +0200

    rework

--- src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
+++ src/test/java/io/github/simple4tests/ohrm/OhrmITs.java
@@ -8,6 +8,7 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
+//@ExtendWith(SerenityJUnit5Extension.class)
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(
