0016e256b1125c024d39a442613e20d7de6e7812
simple4tests <simple4tests@gmail.com>
2026-01-09 14:05:59 +0100

    wip ...

---
+++ src/test/java/io/github/simple4tests/ohrm/glue/ErrorsSteps.java
@@ -0,0 +1,52 @@
+package io.github.simple4tests.ohrm.glue;
+
+import io.cucumber.java8.En;
+import io.github.simple4tests.ohrm.Ohrm;
+import io.github.simple4tests.webdriver.reporters.CucumberJava8Reporter;
+import org.hamcrest.Matchers;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ErrorsSteps implements En {
+
+    @Autowired
+    CucumberJava8Reporter reporter;
+
+    @Autowired
+    Ohrm ohrm;
+
+    public ErrorsSteps() {
+
+        Given("browse to goog site", () -> {
+            reporter.startStep("navigate to goog site step");
+            ohrm.ui.driver.navigate().to("http://www.google.com");
+            ohrm.ui.sleep(1000);
+            reporter.endStep();
+        });
+
+        When("browse without screenshot", () -> {
+            reporter.startStep("empty step");
+            reporter.endStep();
+        });
+
+        When("browse with screenshot", () -> {
+            reporter.startStep("screenshot step");
+            reporter.reportScreenshot();
+            reporter.endStep();
+        });
+
+        When("browse with screenshots", () -> {
+            reporter.startStep("screenshots step");
+            reporter.reportScreenshot();
+            reporter.endStep();
+            reporter.startStep("my double screenshot");
+            reporter.reportScreenshot();
+            reporter.reportScreenshot();
+            reporter.endStep();
+        });
+
+        Then("simulate an error", () ->
+                reporter.assertThat("failed step",
+                        1,
+                        Matchers.equalTo(2)));
+    }
+}
