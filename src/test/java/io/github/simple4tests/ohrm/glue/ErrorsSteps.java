fc6bd87da5077cf2b61c217f075c466b45e0d2a9
simple4tests <simple4tests@gmail.com>
2026-01-09 13:38:54 +0100

    wip ...

--- src/test/java/io/github/simple4tests/ohrm/glue/ErrorsSteps.java
+++ src/test/java/io/github/simple4tests/ohrm/glue/ErrorsSteps.java
@@ -2,19 +2,22 @@ package io.github.simple4tests.ohrm.glue;
 
 import io.cucumber.java8.En;
 import io.github.simple4tests.ohrm.Ohrm;
-import io.github.simple4tests.webdriver.reporters.SerenityReporter;
-import net.serenitybdd.annotations.Shared;
+import io.github.simple4tests.ohrm.context.TestConfig;
+import io.github.simple4tests.ohrm.context.TestData;
+import io.github.simple4tests.webdriver.reporters.CucumberJava8Reporter;
 import org.hamcrest.Matchers;
 
 public class ErrorsSteps implements En {
 
-    @Shared
-    SerenityReporter reporter;
+    CucumberJava8Reporter reporter;
+    TestData testData;
 
-    @Shared
     Ohrm ohrm;
 
-    public ErrorsSteps() {
+    public ErrorsSteps(TestConfig config) {
+        this.reporter = config.reporter;
+        this.ohrm = config.ohrm;
+        this.testData = config.testData;
 
         Given("browse to goog site", () -> {
             reporter.startStep("navigate to goog site step");
